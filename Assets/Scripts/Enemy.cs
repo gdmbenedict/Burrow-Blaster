e4f9d4806be3bcd8139252ce14e065695c7ad342
Matthieu Benedict <97464794+gdmbenedict@users.noreply.github.com>
2024-10-03 11:28:30 -0300

    Enemy Manager
    Made a basic enemy manager that checks if enemies are on screen and updates enemy values.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,6 +5,7 @@ using UnityEngine.UIElements;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private EnemyManager enemyManager;
     [SerializeField] private GameObject scrap;
     [SerializeField] private Weapon weapon;
     private bool onScreen;
@@ -12,19 +13,39 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-
+        enemyManager = FindObjectOfType<EnemyManager>();
+        enemyManager.AddToEnemies(this);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (onScreen)
+        {
+            Attack(enemyManager.GetPlayerLocation());
+        }
+    }
+
+    public void ToggleOnScreen()
+    {
+        onScreen = !onScreen;
+        if (onScreen)
+        {
+            //added in to give player moment before the enemy starts shooting
+            weapon.ActivateCooldown();
+        }
+    }
+
+    public bool GetOnScreen()
+    {
+        return onScreen;
     }
 
     public void Die()
     {
         //instantiate scrap object
         Instantiate(scrap, transform.position, Quaternion.identity);
+        enemyManager.RemoveFromEnemies(this);
         Destroy(gameObject);
     }
 
