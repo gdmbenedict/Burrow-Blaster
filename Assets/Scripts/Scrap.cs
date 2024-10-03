fb9aee7211ecd30d64d3be21d6562a7c2d330947
Matthieu Benedict <97464794+gdmbenedict@users.noreply.github.com>
2024-10-03 10:21:27 -0300

    Scrap Manager
    Created a scrap manager to handle scrap tracking, additions, and transactions.

--- Assets/Scripts/Scrap.cs
+++ Assets/Scripts/Scrap.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Scrap : MonoBehaviour
 {
+    [SerializeField] private int value;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +20,12 @@ public class Scrap : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Destroy(gameObject);
+        ScrapManager scrapManager = other.GetComponent<ScrapManager>();
+
+        if (scrapManager != null)
+        {
+            scrapManager.AddScrap(value);
+            Destroy(gameObject);
+        }   
     }
 }
