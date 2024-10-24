c72c7f4527416e7931ebe850db5c501be705039b
Matthieu Benedict <97464794+gdmbenedict@users.noreply.github.com>
2024-10-24 12:53:46 -0300

    Player Combat Extension
    Extended the player combat class to function with the additional player weapons.

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -1,31 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Build;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerCombat : MonoBehaviour
 {
-    private bool isFiring;
-    [SerializeField] private Weapon weapon;
+    private bool blasterIsFiring;
+    private bool sideShotIsFiring;
+    private bool laserIsFiring;
+
+    [Header("Unlocks")]
+    [SerializeField] private bool sideShotUnlocked;
+    [SerializeField] private bool laserUnlocked;
+
+    [Header("Weapons")]
+    [SerializeField] private Weapon blaster;
+    [SerializeField] private Weapon sideShot;
+    [SerializeField] private Weapon laser;
 
     // Start is called before the first frame update
     void Start()
     {
-        isFiring = false;
+        blasterIsFiring = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isFiring)
+        if (blasterIsFiring)
         {
-            weapon.Fire();
+            blaster.Fire();
+        }
+
+        if (sideShotIsFiring && sideShotUnlocked)
+        {
+            sideShot.Fire();
+        }
+
+        if (laserIsFiring && laserUnlocked)
+        {
+            //laser 
         }
     }
 
-    public void GetFiring(InputAction.CallbackContext input)
+    //method that gets input for the blaster
+    public void GetBlaster(InputAction.CallbackContext input)
     {
-        isFiring = input.action.IsPressed();
+        blasterIsFiring = input.action.IsPressed();
+    }
+
+    //method that gets input for the blaster
+    public void GetSideShot(InputAction.CallbackContext input)
+    {
+        sideShotIsFiring = input.action.IsPressed();
+    }
+
+    //method that gets input for the blaster
+    public void GetLaser(InputAction.CallbackContext input)
+    {
+        laserIsFiring = input.action.IsPressed();
     }
 
 
