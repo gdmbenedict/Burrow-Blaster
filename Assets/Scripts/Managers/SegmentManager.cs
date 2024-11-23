164590100362eba5e309fae4a47bc34dcac1c1df
Matthieu Benedict <97464794+gdmbenedict@users.noreply.github.com>
2024-11-23 18:03:43 -0400

    Added Scene Field
    Added thee SceneField script to add serializable scenes to the scene loader for easier use.

--- Assets/Scripts/Managers/SegmentManager.cs
+++ Assets/Scripts/Managers/SegmentManager.cs
@@ -1,9 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class SegmentLoader : MonoBehaviour
+//Struct to store variables on segments
+[System.Serializable] //makes visible in inspector
+public struct Segment
 {
+    public SceneField scene;
+    public Vector3 position;
+    public bool loaded;
+}
+
+public class SegmentManager : MonoBehaviour
+{
+    [Header("Segments")]
+    [SerializeField] private List<Segment> segments;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +28,11 @@ public class SegmentLoader : MonoBehaviour
     void Update()
     {
         
+    }
+
+    //Function to load segments if player is close to segment position;
+    private void LoadSegment(Segment segment)
+    {
+
     }
 }
