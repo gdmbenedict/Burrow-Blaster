9083aa6bed5eac78a6262bc5ae03b3923841925e
Matthieu Benedict <97464794+gdmbenedict@users.noreply.github.com>
2024-11-25 15:37:06 -0400

    Added Music
    Added a simple music manager to the game as well as some songs for menus and gameplay. (Will probably have to change for sounds for post school release.

--- Assets/Scripts/Managers/MusicManager.cs
+++ Assets/Scripts/Managers/MusicManager.cs
@@ -3,21 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
+[System.Serializable]
+public struct Song
+{
+    public enum SongType
+    {
+        MenuMusic,
+        GameplayMusic,
+        BossMusic,
+        UpgradeMusic,
+        PauseMusic
+    }
+
+    public SongType type;
+    public AudioClip song;
+}
+
 public class MusicManager : MonoBehaviour
 {
     [Header("Music Output")]
     [SerializeField] private AudioSource musicSource;
-    [SerializeField] private AudioMixerGroup musicMixerGroup;
 
-    // Start is called before the first frame update
-    void Start()
+    [Header("Songs")]
+    [SerializeField] private List<Song> songs;
+
+    public void PlayMusic(Song.SongType songType)
     {
-        
+        for (int i=0; i<songs.Count; i++)
+        {
+            if (songs[i].type == songType)
+            {
+                musicSource.clip = songs[i].song;
+                musicSource.Play();
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
