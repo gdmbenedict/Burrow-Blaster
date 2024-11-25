a2cbcfaa8378f6fab446a150b467ac9aba29babd
Matthieu Benedict <97464794+gdmbenedict@users.noreply.github.com>
2024-11-25 06:08:04 -0400

    Volume Options
    Added functionality to volume sliders in the options menus.

---
+++ Assets/Scripts/Managers/MusicManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicManager : MonoBehaviour
+{
+    [Header("Music Output")]
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioMixerGroup musicMixerGroup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Managers/SFXManager.cs
+++ Assets/Scripts/Managers/SFXManager.cs
@@ -2,11 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SFXManager : MonoBehaviour
 {
+    [Header("SFX Output")]
     [SerializeField] private List<AudioSFXPlayer> sfx_Players;
-    
+
+    private void Start()
+    {
+
+    }
+
     //Function that finds the target SFX player and makes it play specified audio-clip
     public void PlaySFX(SFX_Type targetSFX, AudioClip audioClip, bool oneShot)
     {
