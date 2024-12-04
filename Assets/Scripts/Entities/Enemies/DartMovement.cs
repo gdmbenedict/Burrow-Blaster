82c1c105303e4e2e65cd3bf759e828439472d670
Matthieu Benedict <97464794+gdmbenedict@users.noreply.github.com>
2024-12-03 22:43:42 -0400

    Dart Fix
    Dart Movement now works properly

--- Assets/Scripts/Entities/Enemies/DartMovement.cs
+++ Assets/Scripts/Entities/Enemies/DartMovement.cs
@@ -4,13 +4,9 @@ using UnityEngine;
 
 public class DartMovement : MonoBehaviour
 {
-    [SerializeField] private float cameraDist;
     [SerializeField] private float dartRange;
     [SerializeField] private float coolDownTime;
     [SerializeField] private float dartTime;
-    private GameObject camera;
-    private float followSpeed;
-    private IEnumerator dartCoroutine;
     private Vector3 center;
     private bool canCallDart = true;
 
@@ -19,25 +15,12 @@ public class DartMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        camera = Camera.main.gameObject;
-        followSpeed = camera.GetComponent<CameraController>().GetSpeed();
-
-        Debug.Log(transform.position);
-        center = transform.position;
-        //Debug.Log(center);
+        StartCoroutine(GetPositionDelayed());
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-
-        if (center.z - camera.transform.position.z < cameraDist)
-        {
-            Debug.Log("center being updated");
-            float zMovement = followSpeed * Time.fixedDeltaTime;
-            center = new Vector3(center.x, center.y, center.z + zMovement);
-        }
-
         if (canCallDart)
         {
             StartCoroutine(DartToPosition(Random.Range(-dartRange, dartRange), Random.Range(-dartRange, dartRange)));
@@ -53,10 +36,11 @@ public class DartMovement : MonoBehaviour
         //set target location
         xPos += center.x;
         zPos += center.z;
+        Debug.Log(zPos);
         Vector3 targetPos = new Vector3(xPos, center.y, zPos);
         Vector3 startPos = transform.position;
-        Debug.Log(startPos);
-        Debug.Log(targetPos);
+        //Debug.Log("start Pos: " + startPos);
+        //Debug.Log("target Pos: " + targetPos);
 
         //move towards location
         float timer = 0;
@@ -80,5 +64,12 @@ public class DartMovement : MonoBehaviour
         //wait then allow call to behaviour
         yield return new WaitForSeconds(coolDownTime);
         canCallDart = true;
+    }
+
+    private IEnumerator GetPositionDelayed()
+    {
+        yield return null;
+        center = transform.position;
+        Debug.Log(center);
     }
 }
